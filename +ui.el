a819592b32467ff8cde15129adaabf45cc135d93
appleshan <apple.shan@gmail.com>
2018-07-11 13:57:57 +0800

    设置 fullscreen、modeline、minibuffer

--- +ui.el
+++ +ui.el
@@ -1,14 +1,25 @@
 ;;; +ui.el --- description -*- lexical-binding: t; -*-
 
+;; Theme
 (when (featurep! :ui doom)
   (setq doom-theme 'doom-dracula))
 
-;; Font setup
+;; Start maximized
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; Fonts
 (setq
       doom-font (font-spec :family "Fira Mono" :size 16)
       doom-variable-pitch-font (font-spec :family "Fira Sans")
       doom-unicode-font (font-spec :family "DejaVu Sans Mono")
       doom-big-font (font-spec :family "Fira Mono" :size 19))
+
+;; Modeline
+(setq +doom-modeline-buffer-file-name-style 'relative-from-project)
+
+;; Minibuffer
+(setq show-trailing-whitespace t)
+(add-hook! minibuffer-setup (setq-local show-trailing-whitespace nil))
 
 (provide '+ui)
 
