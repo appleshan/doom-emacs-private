91e6f98c07dfa0d1141f832484c125973c7f177c
appleshan <apple.shan@gmail.com>
2018-07-24 15:58:13 +0800

    设置用户信息

--- config.el
+++ config.el
@@ -1,5 +1,11 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
+;; User Credentials
+(setq
+    user-mail-address "apple.shan@gmail.com"
+    user-full-name "Apple Shan")
+(setq auth-sources '("~/.authinfo"))
+
 ;; ------------- Load Personal Modules --------------
 (load! "+bindings")
 (load! "+ui")
