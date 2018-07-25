a18194e0ed4bcbe3ab58b5ad0b67fe584ba35ddf
appleshan <apple.shan@gmail.com>
2018-07-25 11:08:27 +0800

    修改pyim的repo

--- modules/private/chinese/packages.el
+++ modules/private/chinese/packages.el
@@ -2,6 +2,6 @@
 
 (package! cal-china-x)
 (package! pangu-spacing)
-(package! pyim :recipe (:fetcher github :repo "tumashu/pyim"))
-(package! pyim-basedict :recipe (:fetcher github :repo "tumashu/pyim-basedict"))
+(package! pyim)
+(package! pyim-basedict)
 ;(package! pyim-greatdict :recipe (:fetcher github :repo "tumashu/pyim-greatdict"))
