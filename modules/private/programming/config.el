fdae028f25d98a315e8a831aac54595f243119c1
appleshan <apple.shan@gmail.com>
2018-07-26 17:58:00 +0800

    添加函数，功能为显示 ascii编码表

--- modules/private/programming/config.el
+++ modules/private/programming/config.el
@@ -8,3 +8,26 @@
   (define-key yas-minor-mode-map (kbd "\C-c TAB") 'yas-expand)
 
   (set-face-background 'secondary-selection "gray"))
+
+(defun +programming|ascii-table ()
+  "Display basic ASCII table (0 thru 128)."
+  (interactive)
+  (switch-to-buffer "*ASCII*")
+  (erase-buffer)
+  (setq buffer-read-only nil)        ;; Not need to edit the content, just read mode (added)
+  (local-set-key "q" 'bury-buffer)   ;; Nice to have the option to bury the buffer (added)
+  (setq lower32 '("nul" "soh" "stx" "etx" "eot" "enq" "ack" "bel"
+                  "bs" "ht" "lf" "vt" "ff" "cr" "so" "si"
+                  "dle" "dc1" "dc2" "dc3" "dc4" "nak" "syn" "etb"
+                  "can" "em" "sub" "esc" "fs" "gs" "rs" "us"
+                  ))
+  (save-excursion (let ((i -1))
+    (insert "ASCII characters 0 thru 127.\n\n")
+    (insert " Hex  Dec  Char|  Hex  Dec  Char|  Hex  Dec  Char|  Hex  Dec  Char\n")
+    (while (< i 31)
+      (insert (format "%4x %4d %4s | %4x %4d %4s | %4x %4d %4s | %4x %4d %4s\n"
+                      (setq i (+ 1  i)) i (elt lower32 i)
+                      (setq i (+ 32 i)) i (single-key-description i)
+                      (setq i (+ 32 i)) i (single-key-description i)
+                      (setq i (+ 32 i)) i (single-key-description i)))
+      (setq i (- i 96))))))
