f833dc6ebd0ee13edebf8a763451ecc15b1776f9
appleshan <apple.shan@gmail.com>
2018-07-26 17:21:50 +0800

    添加包discover-my-major，显示主模式的快捷键

--- modules/private/appearance/config.el
+++ modules/private/appearance/config.el
@@ -1,5 +1,19 @@
 ;;; config.el -*- lexical-binding: t; -*-
 
+;; Discover key bindings and their meaning for the current Emacs major mode
+(def-package! discover-my-major)
+
 (def-package! on-screen
   :init (require 'on-screen)
   :config (on-screen-global-mode +1))
+
+
+;;
+;; Keybindings
+;;
+
+(map! :leader
+  (:desc "help" :prefix "h"
+    :n "h" help-map
+    :desc "Discover Emacs major mode"              :nv "z"  #'discover-my-major)
+  )
--- modules/private/appearance/packages.el
+++ modules/private/appearance/packages.el
@@ -1,3 +1,4 @@
 ;;; packages.el -*- lexical-binding: t; -*-
 
+(package! discover-my-major)
 (package! on-screen)
