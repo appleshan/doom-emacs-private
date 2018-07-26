c900d7014ec133c12f06a65c07234a905fb08063
Apple Shan <apple.shan@gmail.com>
2018-07-26 22:43:57 +0800

    editor module: Move Current Line Up or Down

--- modules/private/editor/init.el
+++ modules/private/editor/init.el
@@ -10,3 +10,24 @@
                     ("\\.cron\\(tab\\)?\\'" . crontab-mode)
                     ("cron\\(tab\\)?\\." . crontab-mode)))
   (add-to-list 'auto-mode-alist elt-cons))
+
+;; {{ Move Current Line Up or Down
+;; @see http://emacsredux.com/blog/2013/04/02/move-current-line-up-or-down/
+(defun +editor|move-line-up ()
+  "Move up the current line."
+  (interactive)
+  (transpose-lines 1)
+  (forward-line -2)
+  (indent-according-to-mode))
+
+(defun +editor|move-line-down ()
+  "Move down the current line."
+  (interactive)
+  (forward-line 1)
+  (transpose-lines 1)
+  (forward-line -1)
+  (indent-according-to-mode))
+
+(global-set-key (kbd "M-<up>") '+editor|move-line-up)
+(global-set-key (kbd "M-<down>") '+editor|move-line-down)
+;; }}
