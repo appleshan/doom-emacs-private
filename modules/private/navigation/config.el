2a19fe5117d0fd0750c7ccd2e866ca81149710de
appleshan <apple.shan@gmail.com>
2018-07-26 11:29:58 +0800

    使用du统计目录的磁盘大小

--- modules/private/navigation/config.el
+++ modules/private/navigation/config.el
@@ -18,7 +18,20 @@
   (setq directory-free-space-args "-Phk") ;目录空间选项
   (setq dired-auto-revert-buffer t)
 
-  (setq find-ls-option '("-print0 | xargs -0 ls -ald" . "")))
+  (setq find-ls-option '("-print0 | xargs -0 ls -ald" . ""))
+
+  ;; @see http://oremacs.com/2015/01/12/dired-file-size/
+  (defun +dired|get-dir-size ()
+    (interactive)
+    (let ((files (dired-get-marked-files)))
+      (with-temp-buffer
+        (apply 'call-process "/usr/bin/du" nil t nil "-sh" files)
+        (message
+          "Size of all marked files: %s"
+          (progn
+            (re-search-backward "\\(^[ 0-9.,]+[A-Za-z]+\\).*$")
+            (match-string 1))))))
+  )
 
 (after! dired-k
   (setq dired-k-padding 1)
@@ -38,4 +51,5 @@
   :after dired
   :map dired-mode-map
   :n "DEL" #'dired-up-directory
-  :n "R" #'dired-efap)
+  :n "R" #'dired-efap
+  :n "Z" #'+dired|get-dir-size)
