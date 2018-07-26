83d7f1a49ae888a7c8598486e130185173f52b44
Apple Shan <apple.shan@gmail.com>
2018-07-26 22:51:50 +0800

    editor module: scroll functions

--- modules/private/editor/init.el
+++ modules/private/editor/init.el
@@ -11,6 +11,29 @@
                     ("cron\\(tab\\)?\\." . crontab-mode)))
   (add-to-list 'auto-mode-alist elt-cons))
 
+;; {{ scroll functions
+(defun +editor|hold-line-scroll-up()
+  "Scroll the page with the cursor in the same line"
+  (interactive)
+  ;; move the cursor also
+  (let ((tmp (current-column)))
+    (scroll-up 1)
+    (line-move-to-column tmp)
+    (forward-line 1)))
+
+(defun +editor|hold-line-scroll-down()
+  "Scroll the page with the cursor in the same line"
+  (interactive)
+  ;; move the cursor also
+  (let ((tmp (current-column)))
+    (scroll-down 1)
+    (line-move-to-column tmp)
+    (forward-line -1)))
+
+(global-set-key [(control down)] '+editor|hold-line-scroll-up)
+(global-set-key [(control up)] '+editor|hold-line-scroll-down)
+;; }}
+
 ;; {{ Move Current Line Up or Down
 ;; @see http://emacsredux.com/blog/2013/04/02/move-current-line-up-or-down/
 (defun +editor|move-line-up ()
