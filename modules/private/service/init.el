8de7735eab97739388c1ba6980e33a7b439e3f99
appleshan <apple.shan@gmail.com>
2018-07-26 17:39:24 +0800

    Configure network proxy

---
+++ modules/private/service/init.el
@@ -0,0 +1,30 @@
+;;; init.el -*- lexical-binding: t; -*-
+
+;;{{ Configure network proxy
+(defun show-proxy ()
+  "Show http/https proxy."
+  (interactive)
+  (if url-proxy-services
+      (message "Current proxy is \"%s\"" url-proxy-services)
+    (message "No proxy")))
+
+(defun set-proxy ()
+  "Set http/https proxy."
+  (interactive)
+  (setq url-proxy-services `(("http" . "127.0.0.1:18080")
+                             ("https" . "127.0.0.1:18080")))
+  (show-proxy))
+
+(defun unset-proxy ()
+  "Unset http/https proxy."
+  (interactive)
+  (setq url-proxy-services nil)
+  (show-proxy))
+
+(defun toggle-proxy ()
+  "Toggle http/https proxy."
+  (interactive)
+  (if url-proxy-services
+      (unset-proxy)
+    (set-proxy)))
+;;}}
