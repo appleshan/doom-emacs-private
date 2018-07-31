a31096b1384be6439b99b6bc9dd817631d45ba20
appleshan <apple.shan@gmail.com>
2018-07-31 13:20:20 +0800

    orgmode 的列表还包括“+”开头的行，这条正则需要重写

--- modules/private/orgmode/init.el
+++ modules/private/orgmode/init.el
@@ -12,17 +12,6 @@
       ;; elegant.
       org-bullets-bullet-list '("#"))
 
-;;{{ 更好看的符号列表标记
-;; @see https://github.com/lujun9972/emacs-document/blob/master/org-mode/%E5%B0%86org%E7%9C%8B%E6%88%90%E6%96%87%E5%AD%97%E5%A4%84%E7%90%86%E5%99%A8.org
-;; 这段代码将所有行中匹配指定正则表达式的内容都显示为一个Unicode的圆形符号,
-;; 该段正则的意思是“以 1 个或多个破折号开头,紧接着是一个空格”.
-;; 用星号和破折号来作为符号列表的标记挺好的, 但是使用一个真正的圆形符号来作标示也不错:
-(font-lock-add-keywords
- 'org-mode
- '(("^\\([-]\\) "
-    (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
-;;}}
-
 ;; {{ Move single cells using C-M-up C-M-down C-M-left C-M-right
 (add-hook 'org-mode-hook
  '(lambda ()
