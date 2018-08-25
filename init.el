d6ccfb86002e2f2df9901ff4065486383cb21e20
appleshan <apple.shan@gmail.com>
2018-08-25 15:01:57 +0800

    把 org 多余的功能关闭

--- init.el
+++ init.el
@@ -134,11 +134,12 @@
       ;nix               ; I hereby declare "nix geht mehr!"
       ;ocaml             ; an objective camel
        (org              ; organize your plain life in plain text
-        +attach          ; custom attachment system
-        +babel           ; running code in org
-        +capture         ; org-capture in and outside of Emacs
-        +export          ; Exporting org to whatever you want
-        +present)        ; Emacs for presentations
+      ; +attach          ; custom attachment system
+      ; +babel           ; running code in org
+      ; +capture         ; org-capture in and outside of Emacs
+      ; +export          ; Exporting org to whatever you want
+      ; +present         ; Emacs for presentations
+        )
       ;perl              ; write code no one else can comprehend
       ;php               ; perl's insecure younger brother
       ;plantuml          ; diagrams for confusing people more
