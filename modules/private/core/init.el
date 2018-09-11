4092b11a12e5005b6706a15a6e765e218f08d5f9
appleshan <apple.shan@gmail.com>
2018-09-11 18:55:49 +0800

    Feature: 从 shell 拷贝 PATH 到 exec-path

--- modules/private/core/init.el
+++ modules/private/core/init.el
@@ -1,23 +1,9 @@
 ;;; init.el -*- lexical-binding: t; -*-
 
-;; for linux
-(when IS-LINUX
-  (let ((mypaths
-          '("/opt/java/jdk1.8.0_162/bin"
-            "/opt/java/apache-maven-3.5.2/bin"
-            "/home/appleshan/.local/bin"
-            "/home/appleshan/bin"
-            "/usr/local/sbin"
-            "/usr/local/bin"
-            "/usr/bin"
-            "/usr/bin/site_perl"
-            "/usr/bin/vendor_perl"
-            "/usr/bin/core_perl"
-           )))
-    (setq exec-path (append mypaths (list "." exec-directory)))
-
-    (setenv "PATH" (mapconcat 'identity mypaths ":") )
-    (setenv "HOME" "/home/appleshan")
-    (setenv "JAR_PATH" "/opt/java" )
-    )
-  )
+;; set-envs
+(let ((path (shell-command-to-string ". ~/.oh-my-zsh/custom/path.zsh; echo -n $PATH")))
+  (dolist (item (split-string-and-unquote path ":"))
+    (add-to-list 'exec-path item))
+  (setenv "PATH" path)
+  (setenv "HOME" "/home/appleshan")
+  (setenv "JAR_PATH" "/opt/java" ))
