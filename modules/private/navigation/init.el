da688d2a898221ad90cd788d3593ddfb2ad0a567
appleshan <apple.shan@gmail.com>
2018-09-12 17:46:01 +0800

    Clean: 移除“创建文件时自动创建父目录的功能”，doom-emacs 最近提供了

--- modules/private/navigation/init.el
+++ modules/private/navigation/init.el
@@ -1,9 +1,1 @@
 ;;; init.el -*- lexical-binding: t; -*-
-
-;; Automatically create missing directories when creating new files
-(defun +dired|create-non-existent-directory ()
-(let ((parent-directory (file-name-directory buffer-file-name)))
-  (when (and (not (file-exists-p parent-directory))
-             (y-or-n-p (format "Directory `%s' does not exist! Create it?" parent-directory)))
-    (make-directory parent-directory t))))
-(push #'+dired|create-non-existent-directory find-file-not-found-functions)
