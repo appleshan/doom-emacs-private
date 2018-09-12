dc7ae548a87d654050db472fdc6b3874d47e8339
appleshan <apple.shan@gmail.com>
2018-09-12 20:37:09 +0800

    Add: 添加包 tabbar-ruler，打开文件的名称显示中屏幕顶部

--- modules/private/appearance/config.el
+++ modules/private/appearance/config.el
@@ -15,6 +15,27 @@
   :init (require 'on-screen)
   :config (on-screen-global-mode +1))
 
+;; Required by `tabbar-ruler'
+(def-package! mode-icons
+  :defer t
+  :commands mode-icons-mode
+  :config
+  (mode-icons-mode 1))
+
+;; Required by `tabbar-ruler'
+(def-package! tabbar)
+
+(def-package! tabbar-ruler
+  :init
+  (setq tabbar-ruler-tab-height 28)
+  (setq tabbar-ruler-fancy-tab-separator 'contour)
+  (setq tabbar-ruler-global-tabbar t)
+  (setq tabbar-ruler-global-ruler nil)
+  (setq tabbar-ruler-popup-menu t)
+  (setq tabbar-ruler-popup-toolbar t)
+  (setq tabbar-ruler-popup-scrollbar t)
+  :config
+  (tabbar-ruler-group-by-projectile-project))
 
 ;;
 ;; Keybindings
--- modules/private/appearance/packages.el
+++ modules/private/appearance/packages.el
@@ -3,3 +3,7 @@
 (package! doom-modeline)
 (package! discover-my-major)
 (package! on-screen)
+
+(package! mode-icons)
+(package! tabbar)
+(package! tabbar-ruler)
