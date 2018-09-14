3e38e2a32f71052b5dfff11be8239b56ce9d7496
appleshan <apple.shan@gmail.com>
2018-09-14 18:46:15 +0800

    Docs: 删除“传给 ls 的参数”的文档

--- modules/private/navigation/config.el
+++ modules/private/navigation/config.el
@@ -3,17 +3,7 @@
 (after! dired
   ;; Show directory first
   ;; @see http://oremacs.com/2015/01/13/dired-options/
-  ;; 传给 ls 的参数:
-  ;; l: Is the only mandatory one.
-  ;; a: Means to list invisible files.
-  ;; G: Don't show group information. These days, when there are more laptops
-  ;;    than people, the group info is rarely useful.
-  ;; h: Human readable sizes, such as M for mebibytes.
-  ;; 1v: Affects the sorting of digits, hopefully in a positive way.
-  ;; u: sort by access time, newest first
-  ;; --group-directories-first: self-explanatory, I like to have the directories
-  ;; on the top, separate from the files.
-  (setq dired-listing-switches "--group-directories-first -alh1vu")
+  (setq dired-listing-switches "--group-directories-first -alh1vu") ;传给 ls 的参数
 
   (setq directory-free-space-args "-Phk") ;目录空间选项
   (setq dired-auto-revert-buffer t)
