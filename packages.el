a423b15e2488236f93fc9fa0e4195b6e930c1904
appleshan <apple.shan@gmail.com>
2019-12-02 18:12:06 +0800

    Add: Example of package.el

---
+++ packages.el
@@ -0,0 +1,7 @@
+;; -*- no-byte-compile: t; -*-
+;;; projects-private/doom-emacs-private/packages.el
+
+;;; Examples:
+;; (package! some-package)
+;; (package! another-package :recipe (:host github :repo "username/repo"))
+;; (package! builtin-package :disable t)
