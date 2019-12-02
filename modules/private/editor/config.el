d0ef06e467c612868b2df3a94b49c07e526a4ac5
appleshan <apple.shan@gmail.com>
2019-12-02 17:50:42 +0800

    auto-sudoedit-mode

--- modules/private/editor/config.el
+++ modules/private/editor/config.el
@@ -30,7 +30,7 @@
 (def-package! auto-sudoedit
   :config
   ;; Just hook on `find-file-hook', don't hook `dired-mode-hook', it's unnecessary.
-  (add-hook 'find-file-hook 'auto-sudoedit))
+  (add-hook 'find-file-hook (lambda () (auto-sudoedit-mode 1))))
 
 ;; company-mode
 (after! company
