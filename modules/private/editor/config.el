266294066a742ee74ef1124d0579647b4528178d
appleshan <apple.shan@gmail.com>
2021-08-24 23:48:03 +0800

    Fix: 移除 multiple-cursors ，与 evil 冲突。用 evil-mc 替换。

--- modules/private/editor/config.el
+++ modules/private/editor/config.el
@@ -51,46 +51,6 @@
   :config
   (setq company-english-helper-fuzz-search-p t))
 
-;;;useage:
-;; M-x 后输入 mc-mode
-;; 如果开启了 evil 最好先按 i 进入普通模式
-;; 然后使用C-c C-> 全选中后，就可以随意编排了
-(use-package! multiple-cursors
-  :init
-  (setq mc/list-file (concat doom-cache-dir ".mc-lists.el"))
-  :bind (;; multiple-cursors
-         ("C->" . mc/mark-next-like-this) ; 寻找下一个与当前“选中”（region）相匹配的文本并选中匹配并添加新光标.
-         ("C-<" . mc/mark-previous-like-this) ; 类上，但是寻找方向相反.
-         ("C-M->" . mc/skip-to-next-like-this)
-         ("C-M-<" . mc/skip-to-previous-like-this)
-         ("C-c C->" . mc/mark-all-like-this) ; 找到当前buffer中所有匹配并增加光标.
-         ("C-;" . mc/mark-all-symbols-like-this-toggle)
-         ("C-S-<mouse-1>" . mc/add-cursor-on-click) ; 使用鼠标点击添加/删除光标
-         ;; 对多行的region的操作:
-         ("C-c c c" . mc/edit-lines) ; 当前选中的region中的每一行增加一个cursor.
-         ("C-c c e" . mc/edit-ends-of-lines) ; 当前选中的region中的每一行的行尾增加一个cursor.
-         ("C-c c a" . mc/edit-beginnings-of-lines) ; 当前选中的region中的每一行的行首增加一个cursor.
-         :map mc/keymap
-         ("C-|" . mc/vertical-align-with-space)
-         ("C-;" . mc/my-quit)
-         ("M-n" . mc/cycle-forward)
-         ("M-p" . mc/cycle-backward))
-  :config
-  (progn
-    (defun mc/my-quit ()
-      "Quit from mark mode."
-      (interactive)
-      (mc/keyboard-quit)
-      (multiple-cursors-mode 0))
-
-    (defun mc/mark-all-symbols-like-this-toggle ()
-      "Toogle when only one matches!"
-      (interactive)
-      (if (region-active-p)
-          (mc/my-quit)
-        (mc/mark-all-symbols-like-this)))
-  ))
-
 (use-package! visual-regexp
   ; :commands (vr/replace vr/query-replace) ; See the bind of init-visual-regexp-steroids.
   :defer t)
--- modules/private/editor/packages.el
+++ modules/private/editor/packages.el
@@ -7,8 +7,6 @@
 (package! company-english-helper
   :recipe (:host github :repo "manateelazycat/company-english-helper" :files (:defaults "*")))
 
-(package! multiple-cursors)
-
 ;; 可视化正则匹配
 ;; https://github.com/benma/visual-regexp.el
 ;; https://github.com/benma/visual-regexp-steroids.el
