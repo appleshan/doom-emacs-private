d7656293f22729a351b1886c9d1b26c1150d695f
appleshan <apple.shan@gmail.com>
2021-08-25 21:36:19 +0800

    Fix: 1

--- modules/private/chinese/init.el
+++ modules/private/chinese/init.el
@@ -6,23 +6,21 @@
   (interactive)
 
   (cond
-   ((and (boundp 'evil-mode) evil-mode)
-    ;; evil-mode
-    (cond
-     ((eq evil-state 'insert)
-      (toggle-input-method))
-     (t
-      (evil-insert-state)
-      (unless current-input-method
-        (toggle-input-method))
-      ))
-    (if current-input-method
+    ((and (boundp 'evil-mode) evil-mode)
+      ;; evil-mode
+      (cond
+        ((eq evil-state 'insert)
+          (toggle-input-method))
+        (t
+          (evil-insert-state)
+          (unless current-input-method
+            (toggle-input-method))))
+      (if current-input-method
         (message "IME on!")
-      (message "IME off!"))
-    )
-   (t
-    ;; NOT evil-mode, some guy don't use evil-mode at all
-    (toggle-input-method))))
+        (message "IME off!")))
+    (t
+      ;; NOT evil-mode, some guy don't use evil-mode at all
+      (toggle-input-method))))
 
 (defadvice evil-insert-state (around evil-insert-state-hack activate)
   ad-do-it
