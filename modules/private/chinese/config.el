55bb24694d572a7eb2af6ad34875a8848bc6a886
appleshan <apple.shan@gmail.com>
2021-08-26 22:21:59 +0800

    Fix: 处理 cal-china-x 没有显示中文假日的问题

--- modules/private/chinese/config.el
+++ modules/private/chinese/config.el
@@ -5,10 +5,6 @@
 
 ;; Chinese calendar
 (use-package! cal-china-x
-  ;:defer t
-  ;:init (require 'cal-china-x)
-  :commands cal-china-x-setup
-  :init (add-hook 'calendar-load-hook #'cal-china-x-setup)
   :config
   (progn
     ;; `S' can show the time of sunrise and sunset on Calendar
@@ -22,40 +18,53 @@
     (setq hebrew-holidays nil)    ;; 不显示希伯来人的节日
     (setq islamic-holidays nil)   ;; 不显示伊斯兰教的节日
 
-    ;; Holidays
+    ;; 在日历中显示假日
     (setq calendar-mark-holidays-flag t)
+    ;; 按中国习惯，周一为每周第一天
+    (setq calendar-week-start-day 1)
 
     (setq cal-china-x-important-holidays cal-china-x-chinese-holidays)
     (setq cal-china-x-general-holidays
-          '(;; 农历节日
+          '(;;公历节日
+            (holiday-fixed 1 1 "元旦")
+            (holiday-fixed 3 8 "妇女节")
+            (holiday-fixed 3 12 "植树节")
+            (holiday-fixed 5 1 "劳动节")
+            (holiday-fixed 5 4 "青年节")
+            (holiday-fixed 6 1 "儿童节")
+            (holiday-fixed 9 10 "教师节")
+            (holiday-fixed 10 1 "国庆节")
+            (holiday-fixed 10 2 "国庆节")
+            (holiday-fixed 10 3 "国庆节")
+            ;;农历节日
+            (holiday-chinese-new-year)
+            (holiday-lunar 12 30 "除夕" 0)
             (holiday-lunar 1 1 "春节" 0)
             (holiday-lunar 1 2 "春节" 0)
             (holiday-lunar 1 3 "春节" 0)
             (holiday-lunar 1 15 "元宵节 (正月十五)")
             (holiday-solar-term "清明" "清明节")
             (holiday-lunar 5 5 "端午节 (五月初五)" 0)
-            (holiday-lunar 7 7 "七夕情人节 (七月初七)")
+            (holiday-lunar 7 7 "七夕节 (七月初七)")
             (holiday-lunar 8 15 "中秋节 (八月十五)" 0)
             (holiday-lunar 9 9 "重阳节 (九月初九)" 0)
             (holiday-lunar 12 8 "腊八节" 0)
-            (holiday-lunar 12 22 "冬至" 0)
-            (holiday-fixed 3 8 "妇女节")
-            (holiday-fixed 3 12 "植树节")
-            (holiday-fixed 5 4 "青年节")
-            (holiday-fixed 6 1 "儿童节")
-            (holiday-fixed 9 10 "教师节")))
+            (holiday-lunar 12 22 "冬至" 0)))
+
     (setq holiday-other-holidays
-          '((holiday-fixed 2 14 "情人节")
+          '(;;其它节日
+            (holiday-fixed 2 14 "情人节")
             (holiday-fixed 4 1 "愚人节")
             (holiday-fixed 12 25 "圣诞节")
             (holiday-float 5 0 2 "母亲节")
             (holiday-float 6 0 3 "父亲节")
             (holiday-float 11 4 4 "感恩节")
-            ;; 生日 -- 家人,朋友
+            ;;纪念日，已放入 memorial-day.org，此处注释备存
             (holiday-fixed 11 18 "女儿生日")
             (holiday-lunar 4 25 "老婆生日" 0)
             (holiday-lunar 7 10 "我的生日" 0)
             ))
+
     ;; 只显示我定制的节日
     (setq calendar-holidays
           (append cal-china-x-important-holidays
