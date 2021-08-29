2acf403d61ebeb7e9befe5015bad215b239461fb
appleshan <apple.shan@gmail.com>
2021-08-29 23:43:49 +0800

    Clean: 1

--- init.el
+++ init.el
@@ -18,13 +18,10 @@
 ;; ELPA: refer to https://elpa.emacs-china.org/
 ;;
 ;; ELPA                   镜像地址
-;; GNU ELPA               http://elpa.emacs-china.com/gnu/
-;; MELPA                  http://elpa.emacs-china.com/melpa/
-;; MELPA Stable           http://elpa.emacs-china.com/melpa-stable/
-;; Marmalade              http://elpa.emacs-china.com/marmalade/
-;; Org                    http://elpa.emacs-china.com/org/
-;; Sunrise Commander ELPA http://elpa.emacs-china.com/sunrise-commander/
-;; user42 ELPA            http://elpa.emacs-china.com/user42/
+;; GNU ELPA               http://elpa.emacs-china.org/gnu/
+;; MELPA                  http://elpa.emacs-china.org/melpa/
+;; MELPA Stable           http://elpa.emacs-china.org/stable-melpa/
+;; Org                    http://elpa.emacs-china.org/org/
 ;;
 (setq package-archives
     '(
@@ -32,7 +29,6 @@
       ("melpa-cn"  . "https://elpa.emacs-china.org/melpa/")
       ("gnu-cn"    . "https://elpa.emacs-china.org/gnu/")
       ("org-cn"    . "https://elpa.emacs-china.org/org/")
-    ; ("user42-cn" . "https://elpa.emacs-china.org/user42/")
 
       ;; 清华大学 TUNA 协会开源软件镜像站
     ; ("melpa-cn" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
