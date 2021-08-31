5a42f65fb7542f4767b287052178f9bfd998f23c
appleshan <apple.shan@gmail.com>
2021-08-31 21:03:16 +0800

    Add: 在 chinese 模块添加节日与节日英文名称

--- modules/private/chinese/config.el
+++ modules/private/chinese/config.el
@@ -28,22 +28,21 @@
     (setq cal-china-x-general-holidays
           '(;;公历节日
             (holiday-fixed 1 1 "元旦")
-            (holiday-fixed 3 8 "妇女节")
-            (holiday-fixed 3 12 "植树节")
-            (holiday-fixed 5 1 "劳动节")
-            (holiday-fixed 5 4 "青年节")
-            (holiday-fixed 6 1 "儿童节")
-            (holiday-fixed 9 10 "教师节")
-            (holiday-fixed 10 1 "国庆节")
-            (holiday-fixed 10 2 "国庆节")
-            (holiday-fixed 10 3 "国庆节")
+            (holiday-fixed 3 8 "妇女节")     ; Women's Day
+            (holiday-fixed 3 12 "植树节")    ; Arbor Day
+            (holiday-fixed 5 1 "劳动节")     ; International Workers' Day
+            (holiday-fixed 5 4 "青年节")     ; Chinese Youth Day
+            (holiday-fixed 6 1 "儿童节")     ; Children's Day
+            (holiday-fixed 9 10 "教师节")    ; Teachers' Day
+            (holiday-fixed 10 1 "国庆节")    ; National Day
+            (holiday-fixed 10 24 "程序员节") ; Programmers' Day
             ;;农历节日
             (holiday-chinese-new-year)
             (holiday-lunar 12 30 "除夕" 0)
             (holiday-lunar 1 1 "春节" 0)
             (holiday-lunar 1 2 "春节" 0)
             (holiday-lunar 1 3 "春节" 0)
-            (holiday-lunar 1 15 "元宵节 (正月十五)")
+            (holiday-lunar 1 15 "元宵节 (正月十五)") ; Lantern Festival
             (holiday-solar-term "清明" "清明节")
             (holiday-lunar 5 5 "端午节 (五月初五)" 0)
             (holiday-lunar 7 7 "七夕节 (七月初七)")
@@ -56,10 +55,17 @@
           '(;;其它节日
             (holiday-fixed 2 14 "情人节")
             (holiday-fixed 4 1 "愚人节")
+            (holiday-fixed 4 22 "世界地球日") ; Earth Day
+            (holiday-fixed 4 23 "世界读书日") ; World Book Day
             (holiday-fixed 12 25 "圣诞节")
             (holiday-float 5 0 2 "母亲节")
             (holiday-float 6 0 3 "父亲节")
             (holiday-float 11 4 4 "感恩节")
+            (holiday-sexp '(if (or (zerop (% year 400))
+                                   (and (% year 100) (zerop (% year 4))))
+                               (list 9 12 year)
+                             (list 9 13 year))
+                         "俄罗斯的程序员节") ; World Programmers' Day
             ;;纪念日，已放入 memorial-day.org，此处注释备存
             (holiday-fixed 11 18 "女儿生日")
             (holiday-lunar 4 25 "老婆生日" 0)
