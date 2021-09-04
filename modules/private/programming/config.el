686e058499ad2dae851088884dadb962d247b469
appleshan <apple.shan@gmail.com>
2021-09-04 18:09:11 +0800

    Add: 更好的进入与退出 magit-status
    这段代码让magit-status在frame中单独运行，然后退出magit时恢复旧的窗口配置。

--- modules/private/programming/config.el
+++ modules/private/programming/config.el
@@ -37,6 +37,27 @@
 ;; pretty-magit
 ;; @see http://www.modernemacs.com/post/pretty-magit/
 (after! magit
+  ;; {{ This code makes magit-status run alone in the frame,
+  ;; and then restores the old window configuration when you quit out of magit.
+
+  ;full screen magit-status
+  (defadvice magit-status (around magit-fullscreen activate)
+    (window-configuration-to-register :magit-fullscreen)
+    ad-do-it
+    (delete-other-windows))
+
+  (defun magit-quit-session ()
+    "Restores the previous window configuration and kills the magit buffer"
+    (interactive)
+    (kill-buffer)
+    (jump-to-register :magit-fullscreen))
+
+  (undefine-key! magit-mode-map "q")
+  (define-key magit-mode-map "q" #'magit-quit-session)
+  (define-key! 'normal (magit-mode-map) "q" #'magit-quit-session)
+  (define-key! 'visual (magit-mode-map) "q" #'magit-quit-session)
+  ;; }}
+
   ;; {{ C-c C-a to amend without any prompt
   (defun magit-just-amend ()
     (interactive)
